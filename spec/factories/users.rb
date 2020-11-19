3eaeda4b2a765f8ad28658cb35e1e5944e3077a5
suzuki-shinsuke <73277727+suzuki-shinsuke@users.noreply.github.com>
2020-11-19 19:28:59 +0900

    ユーザー管理機能実装（修正）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'1a' + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"山田"}
-    first_name            {"太朗"}
-    last_name_kana        {"ヤマダ"}
-    first_name_kana       {"タロウ"}
-    birth_date            {Faker::Date.between(from: 2.days.ago, to: Date.today)}
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太朗' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_date            { Faker::Date.between(from: 2.days.ago, to: Date.today) }
   end
 end
