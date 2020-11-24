0c734d7ccc3d8c1c393b611591f7f2e075bbb9c0
suzuki-shinsuke <73277727+suzuki-shinsuke@users.noreply.github.com>
2020-11-24 20:13:11 +0900

    itemモデル単体テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品新規登録' do
+    context '商品登録がうまくいく時' do
+      it 'name, info, price, image と、category_id, status_id, shipping_fee_id, shipping_area_id, scheduled_delivery_id と user_id が存在する' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品登録がうまくいかない時' do
+      it 'nameが空' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'imageが空' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'priceが空' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'category_idが未選択' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 0')
+      end
+      it 'status_idが未選択' do
+        @item.status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status must be other than 0')
+      end
+      it 'shipping_fee_idが未選択' do
+        @item.shipping_fee_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee must be other than 0')
+      end
+      it 'shipping_area_idが未選択' do
+        @item.shipping_area_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping area must be other than 0')
+      end
+      it 'scheduled_delivery_idが未選択' do
+        @item.scheduled_delivery_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Scheduled delivery must be other than 0')
+      end
+      it 'priceが300以下' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが9,999,999以上' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it 'priceが全角英数' do
+        @item.price = '７６８'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price には半角数字を使用してください', 'Price is not a number')
+      end
+    end
+  end
 end
