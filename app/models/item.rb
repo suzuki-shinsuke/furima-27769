7c4ff3cece5a9015ef03d67c485fd22369987997
suzuki-shinsuke <73277727+suzuki-shinsuke@users.noreply.github.com>
2020-11-24 18:05:33 +0900

    商品出品機能実装

---
+++ app/models/item.rb
@@ -0,0 +1,34 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :shipping_area
+  belongs_to :scheduled_delivery
+
+  belongs_to :user
+  has_one :buy
+  has_one_attached :image
+
+  
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :image
+    validates :price
+  end
+  
+  with_options numericality: {other_than: 0} do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :scheduled_delivery_id
+  end
+  
+
+
+
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  validates :price, format: {with: /\A[0-9]+\z/, message: "には半角数字を使用してください"}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :buys
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
