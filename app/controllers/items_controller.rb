cf36d1a4269c970658da70f458eab7b1be7a3c10
suzuki-shinsuke <73277727+suzuki-shinsuke@users.noreply.github.com>
2020-11-25 21:42:19 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,16 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
 
+
   def new
     @item = Item.new
   end
+
 
   def create
     @item = Item.new(item_params)
@@ -18,6 +22,14 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+
 
   private
 
