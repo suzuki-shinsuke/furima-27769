00130b31a764e40e3d0eb1d3fafff0a999919ba7
suzuki-shinsuke <73277727+suzuki-shinsuke@users.noreply.github.com>
2020-11-26 16:14:07 +0900

    Revert "商品情報編集機能実装"
    This reverts commit ec0417a6396287e65fa25e9ae272ad5b0035fb1b.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: :new
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -11,7 +11,9 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       render :new
@@ -20,20 +22,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user.id
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
   end
 
   private
